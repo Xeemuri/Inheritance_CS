78d5aca1ba026a7d2336e32323d0d02901ac2ad5
Илья Щербаков <iliya309@mail.ru>
2026-06-03 19:21:49 +1000

    Add project files.

---
+++ Academy/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human = new Human("Henriksson", "Martin", 35);
+            //human.Info();
+            Console.WriteLine(human);
+
+            Student student = new Student("Pinkman", "Jesse", 22, "Chemistry", "CS-01", 90, 95);
+            Console.WriteLine(student);
+            Teacher teacher = new Teacher
+                (
+                    "White", "Walter", 50,
+                    "Chemistry", 25
+                );
+            Console.WriteLine(teacher);
+        }
+    }
+}
